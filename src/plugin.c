442f1e409a0a39dd1014edd40e1aac9cafdec5db
Bluscream <admin@timo.de.vc>
2018-01-12 09:39:10 +0100

    3.0.18.1

--- src/plugin.c
+++ src/plugin.c
@@ -18,7 +18,7 @@
 #include "teamspeak/public_definitions.h"
 #include "teamspeak/public_rare_definitions.h"
 #include "teamspeak/clientlib_publicdefinitions.h"
-#include "../../ts3_functions.h"
+#include "ts3_functions.h"
 #include "plugin.h"
 
 static struct TS3Functions ts3Functions;
